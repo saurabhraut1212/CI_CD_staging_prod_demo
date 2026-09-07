2521c4f1450347dacb02494c684ba94c036347d4
saurabhraut1212 <saurabhrmahadev1212@gmail.com>
2026-09-07 11:28:21 +0530

    feat:make error for test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -9,6 +9,6 @@ describe('App', () => {
     const button = screen.getByRole('button', { name: /count is 0/i })
     expect(button).toBeInTheDocument()
     await userEvent.click(button)
-    expect(screen.getByRole('button', { name: /count is 1/i })).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /count is 2/i })).toBeInTheDocument()
   })
 })
